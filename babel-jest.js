1130366cde0b71b247b0f6787a1bd2c567a859c2
Adrian Moennich <adrian.moennich@cern.ch>
2019-07-03 11:30:15 +0200

    Run eslint on example/config files as well

--- babel-jest.js
+++ babel-jest.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/unambiguous, import/no-commonjs */
+
 const path = require('path');
 const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
-    configFile: path.resolve(__dirname, '.babelrc'),
+  configFile: path.resolve(__dirname, '.babelrc'),
 });
--- example/webpack.config.babel.js
+++ example/webpack.config.babel.js
@@ -1,37 +1,37 @@
 import {execSync} from 'child_process';
 import path from 'path';
 
-
 // Build JSON URL map - you could move this to a custom build script
 // that writes the URL map to a file and then just import/require it
 // in here instead of generating it whenever webpack is executed
-const urlMap = JSON.parse(execSync('flask url_map_to_json', {
-    env: Object.assign({FLASK_APP: 'app.py'}, process.env),
-}));
-
+const urlMap = JSON.parse(
+  execSync('flask url_map_to_json', {
+    env: {FLASK_APP: 'app.py', ...process.env},
+  })
+);
 
 export default {
-    mode: 'development',
-    devtool: 'source-map',
-    entry: './js/app.js',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        // enable the flask-urls babel plugin and provide it
-                        // with our flask URL map
-                        plugins: [['flask-urls', {urlMap}]]
-                    }
-                }
-            }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'static/dist'),
-        filename: 'app.bundle.js'
-    }
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './js/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // enable the flask-urls babel plugin and provide it
+            // with our flask URL map
+            plugins: [['flask-urls', {urlMap}]],
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'static/dist'),
+    filename: 'app.bundle.js',
+  },
 };
